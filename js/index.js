3040550bf3e12354e6db078d1a6d651690542caa
abbyqin <1213335319@qq.com>
2017-12-17 15:56:32 +0800

    armor

---
+++ js/index.js
@@ -0,0 +1,25 @@
+var navHeight = $(".navbar").height();
+$(function(){
+	$(window).scroll(function(){
+		var sTop = $(document).scrollTop();
+		if(sTop>navHeight){
+			$(".navbar").addClass('nav-fixed');
+		}else{
+			$(".navbar").removeClass('nav-fixed');
+		}
+	});
+	$(".navbar-header").on("click",function(){
+		var isOpen = $(".navbar-collapse").hasClass('in');
+		if(!isOpen){
+			$(".navbar-header").css({
+				"background-color": '#001033'			
+			});
+		}else{
+			$(".navbar-header").css({
+				"background-color": 'transparent'			
+			});
+		}
+	})
+})
+
+
